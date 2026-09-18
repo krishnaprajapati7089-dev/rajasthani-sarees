2c9519cbc90d4460c6c9b833b65e6b9e7dff40bb
krishnaprajapati7089-dev <krishnaprajapati7089@gmail.com>
2026-09-18 11:23:14 +0530

    CROS Fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cros({
+  origin: "https://rajasthani-sarees.vercel.app/"
+}))
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 

66e65ec9dbabe9d10e8de10e128e41a362d6d321
krishnaprajapati7089-dev <krishnaprajapati7089@gmail.com>
2026-09-19 20:44:13 +0530

    Add barcode scanning and update API

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cros({
-  origin: "https://rajasthani-sarees.vercel.app/"
-}))
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
